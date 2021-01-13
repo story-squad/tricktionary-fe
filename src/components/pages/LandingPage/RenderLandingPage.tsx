066efbfa4efc03c53077115b82f892c204abe742
Hailey Hansard <haileyhansard@gmail.com>
2021-01-12 23:57:14 -0800

    initial setup and render of LandingPage

---
+++ src/components/pages/LandingPage/RenderLandingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RenderLandingPage = (): React.ReactElement => {
+  return (
+    <div className="landing-page">
+      <h1>Lets Play!</h1>
+    </div>
+  );
+};
+
+export default RenderLandingPage;
