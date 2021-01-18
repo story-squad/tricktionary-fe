a9e8bbf7eedf668e33fa6df2576a7b3eb9876549
Drew Ross <mail.drew.ross@gmail.com>
2021-01-18 14:38:33 -0800

    Add Writing import/export.

--- src/components/pages/GameContainer/Game/index.ts
+++ src/components/pages/GameContainer/Game/index.ts
@@ -1,5 +1,6 @@
 import Lobby from './Lobby';
 import Pregame from './Pregame';
 import PlayerList from './PlayerList';
+import Writing from './Writing';
 
-export { Lobby, Pregame, PlayerList };
+export { Lobby, Pregame, PlayerList, Writing };
