698f0d91f5b6111e670efef51216441bd4c78078
Drew Ross <mail.drew.ross@gmail.com>
2021-01-18 15:12:36 -0800

    Implement handleSubmitDefinition on form submit. Render word to screen.

--- src/components/pages/GameContainer/Game/Writing.tsx
+++ src/components/pages/GameContainer/Game/Writing.tsx
@@ -10,7 +10,9 @@ const Writing = (props: WritingProps): React.ReactElement => {
   return (
     <div className="pregame game-page">
       <h2>Writing</h2>
-      <form>
+      <h3>Your Word:</h3>
+      <p>{props.lobbyData.word}</p>
+      <form onSubmit={(e) => props.handleSubmitDefinition(e, definition)}>
         <label htmlFor="definition">Write Your Definition:</label>
         <input
           type="textfield"
@@ -27,4 +29,8 @@ export default Writing;
 
 interface WritingProps {
   lobbyData: any;
+  handleSubmitDefinition: (
+    e: React.FormEvent<HTMLFormElement>,
+    definition: string,
+  ) => void;
 }
