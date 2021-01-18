4fa719933e6c7759a0a1d741b3a174e121f281c1
Drew Ross <mail.drew.ross@gmail.com>
2021-01-18 12:50:59 -0800

    Add name, lobbyCode inputs, buttons for New/Join game. Pass props for all except handleJoinLobby.

--- src/components/pages/GameContainer/GameComponents/Lobby.tsx
+++ src/components/pages/GameContainer/GameComponents/Lobby.tsx
@@ -1,11 +1,36 @@
 import React, { useEffect, useState } from 'react';
 
-const Lobby = (): React.ReactElement => {
+const Lobby = (props: LobbyProps): React.ReactElement => {
   return (
     <div className="lobby game-page">
       <h2>Lobby</h2>
+      <label htmlFor="username">Name</label>
+      <input
+        id="username"
+        name="username"
+        value={props.username}
+        onChange={props.handleChangeUsername}
+      />
+      <label htmlFor="lobby-code">Room Code</label>
+      <input
+        id="lobby-code"
+        name="lobby-code"
+        value={props.lobbyCode}
+        onChange={props.handleChangeCode}
+        maxLength={4}
+      />
+      <button onClick={props.handleCreateLobby}>New Game</button>
+      <button>Join Game</button>
     </div>
   );
 };
 
 export default Lobby;
+
+interface LobbyProps {
+  username: string;
+  lobbyCode: string;
+  handleChangeUsername: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleChangeCode: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleCreateLobby: (e: React.MouseEvent) => void;
+}
