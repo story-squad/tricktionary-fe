eec59c59215d48b600325f435c0deb61060a375b
Drew Ross <mail.drew.ross@gmail.com>
2021-01-18 16:55:38 -0800

    Add Guessing import/export.

--- src/components/pages/GameContainer/Game/index.ts
+++ src/components/pages/GameContainer/Game/index.ts
@@ -2,5 +2,6 @@ import Lobby from './Lobby';
 import Pregame from './Pregame';
 import PlayerList from './PlayerList';
 import Writing from './Writing';
+import Guessing from './Guessing';
 
-export { Lobby, Pregame, PlayerList, Writing };
+export { Lobby, Pregame, PlayerList, Writing, Guessing };
