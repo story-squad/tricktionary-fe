264a2a1601c6c086fba85a164c3bda6ccee25f95
Drew Ross <mail.drew.ross@gmail.com>
2021-01-19 11:17:53 -0800

    Add logic to change player className based on submission state for each phase.

--- src/components/pages/GameContainer/Game/PlayerList.tsx
+++ src/components/pages/GameContainer/Game/PlayerList.tsx
@@ -1,11 +1,47 @@
 import React from 'react';
 
+// Functions to determine if the player has submitted, based on the current phase/lobbyData
+const guessArrayContainsPlayer = (guesses: any[], playerId: string) => {
+  let found = false;
+  for (let i = 0; i < guesses.length; i++) {
+    if (guesses[i].player === playerId) {
+      found = true;
+      break;
+    }
+  }
+  return found;
+};
+
+const playerHasSubmitted = (lobbyData: any, player: PlayerItem) => {
+  if (lobbyData.phase === 'WRITING' && player.definition !== '') {
+    return true;
+  } else if (
+    lobbyData.phase === 'GUESSING' &&
+    guessArrayContainsPlayer(lobbyData.guesses, player.id)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const playerClassName = (lobbyData: any, player: PlayerItem) => {
+  return `player${playerHasSubmitted(lobbyData, player) ? ' submitted' : ''}`;
+};
+
 const PlayerList = (props: PlayerListProps): React.ReactElement => {
   return (
     <div className="player-list">
-      {props.players.map((player: PlayerItem) => (
-        <p key={player.id}>{player.username}</p>
-      ))}
+      {props.lobbyData.players.map((player: PlayerItem) => {
+        return (
+          <p
+            className={playerClassName(props.lobbyData, player)}
+            key={player.id}
+          >
+            {player.username}
+          </p>
+        );
+      })}
     </div>
   );
 };
@@ -13,10 +49,11 @@ const PlayerList = (props: PlayerListProps): React.ReactElement => {
 export default PlayerList;
 
 interface PlayerListProps {
-  players: any[];
+  lobbyData: any;
 }
 
 interface PlayerItem {
   id: string;
   username: string;
+  definition: string;
 }
