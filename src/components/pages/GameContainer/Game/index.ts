c39ee8495f376f96799133c3d03716760e30f16e
Drew Ross <mail.drew.ross@gmail.com>
2021-01-19 14:16:51 -0800

    Add Postgame import/export.

--- src/components/pages/GameContainer/Game/index.ts
+++ src/components/pages/GameContainer/Game/index.ts
@@ -3,5 +3,6 @@ import Pregame from './Pregame';
 import PlayerList from './PlayerList';
 import Writing from './Writing';
 import Guessing from './Guessing';
+import Postgame from './Postgame';
 
-export { Lobby, Pregame, PlayerList, Writing, Guessing };
+export { Lobby, Pregame, PlayerList, Writing, Guessing, Postgame };
