879fab767a6b3e0500f6e96e99c06fc22a1dc527
Drew Ross <mail.drew.ross@gmail.com>
2021-01-20 12:40:24 -0800

    Create ReactionPicker component that displays a list of reactions that user can pick from.

---
+++ src/components/common/ReactionPicker/ReactionPicker.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+const ReactionPicker = (props: ReactionPickerProps): React.ReactElement => {
+  const { reactions, cb, definitionId } = props;
+  const [modalOn, setModalOn] = useState(false);
+
+  const pickReaction = (id: number) => {
+    setModalOn(false);
+    cb({ id, definitionId });
+  };
+
+  return (
+    <div className="reaction-picker">
+      <button onClick={() => setModalOn(!modalOn)}>Reaction Picker</button>
+      {modalOn && (
+        <div className="reaction-modal">
+          {reactions.map((reaction) => (
+            <Reaction
+              key={reaction.id}
+              reaction={reaction}
+              pickReaction={pickReaction}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const Reaction = (props: ReactionProps): React.ReactElement => {
+  const { reaction, pickReaction } = props;
+  return (
+    <button className="reaction" onClick={() => pickReaction(reaction.id)}>
+      {reaction.content}
+    </button>
+  );
+};
+
+export default ReactionPicker;
+
+interface ReactionPickerProps {
+  reactions: ReactionItem[];
+  cb: (arg0: CbItem) => void;
+  definitionId: number;
+}
+
+interface ReactionProps {
+  reaction: ReactionItem;
+  pickReaction: (id: number) => void;
+}
+
+export interface ReactionItem {
+  id: number;
+  content: string;
+}
+
+interface CbItem {
+  id: number;
+  definitionId: number;
+}
