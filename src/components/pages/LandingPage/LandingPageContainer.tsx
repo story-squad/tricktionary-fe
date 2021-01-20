ad7074ef8edc938dab8e80a56c837f21b4708dbb
Drew Ross <mail.drew.ross@gmail.com>
2021-01-19 16:13:49 -0800

    Remove unused LandingPage.

--- src/components/pages/LandingPage/LandingPageContainer.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import RenderLandingPage from './RenderLandingPage';
-
-const LandingPageContainer = (): React.ReactElement => {
-  return <RenderLandingPage />;
-};
-
-export default LandingPageContainer;
