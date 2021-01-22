05f7b881a661d8c6ab5941061890d18838361834
Drew Ross <mail.drew.ross@gmail.com>
2021-01-21 16:41:40 -0800

    Wrap GameContainer in Switch/Route.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import { GameContainer } from './components/pages/GameContainer';
 
 const App = (): React.ReactElement => {
   return (
     <div className="App">
-      <GameContainer />
+      <Switch>
+        <Route path="/" component={GameContainer} />
+      </Switch>
     </div>
   );
 };
