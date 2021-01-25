51b9aa9d84ef7ce493e8eb2ed90367bc389eef38
Drew Ross <mail.drew.ross@gmail.com>
2021-01-25 15:18:34 -0800

    Remove unused import.

--- src/components/pages/GameContainer/Game/Writing.tsx
+++ src/components/pages/GameContainer/Game/Writing.tsx
@@ -1,4 +1,4 @@
-import React, { SetStateAction, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { LobbyData } from '../gameTypes';
 
 const Writing = (props: WritingProps): React.ReactElement => {
