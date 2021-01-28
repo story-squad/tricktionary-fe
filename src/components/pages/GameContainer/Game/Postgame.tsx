61eb58ce7737e15d305de65737101d02762925fd
Drew Ross <mail.drew.ross@gmail.com>
2021-01-28 11:18:16 -0800

    Fix getSortedDefinitions function. Subscribe component to guessesState atom.

--- src/components/pages/GameContainer/Game/Postgame.tsx
+++ src/components/pages/GameContainer/Game/Postgame.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
-import { LobbyData, PlayerItem } from '../gameTypes';
+import { useRecoilValue } from 'recoil';
+import { guessesState } from '../../../../state/guessesState';
+import { GuessItem, LobbyData, PlayerItem } from '../gameTypes';
 
 const getSortedDefinitions = (
   lobbyData: LobbyData,
+  guesses: GuessItem[],
   playerDict: PlayerDictionary,
 ): DefinitionResult[] => {
   // Create a definition dictionary to easily map all player guesses to each definition
@@ -16,25 +19,41 @@ const getSortedDefinitions = (
       points: 0,
     };
   });
+  console.log('1');
+  // Add real definition
+  definitions[0] = {
+    username: 'Real Definition',
+    definition: lobbyData.definition,
+    definitionId: 0,
+    guesses: [],
+    points: 0,
+  };
   // Add player guesses to corresponding definitions and increment points earned
-  lobbyData.guesses.forEach((guess) => {
-    definitions[guess.guess].guesses.push(playerDict[guess.player]);
-    definitions[guess.guess].points += 1;
+  guesses.forEach((guess) => {
+    try {
+      definitions[guess.guess].guesses.push(playerDict[guess.player]);
+      definitions[guess.guess].points += 1;
+    } catch {
+      return;
+    }
   });
+  console.log('2: ', definitions);
   // Get an array from the result that can be sorted and mapped in JSX
   let definitionArray = Object.values(definitions);
+  console.log('3: ', definitionArray);
   // Grab the real definition to place at the end after the array is sorted
   const realDefinition = definitionArray.filter(
-    (definition) => definition.id === 0,
+    (definition) => definition.definitionId === 0,
   );
+  console.log('3.5: ', realDefinition);
   // Remove the real definition and sort by point values
   definitionArray = definitionArray
-    .filter((definition) => {
-      definition.definitionId !== 0;
-    })
-    .sort((definition) => definition.points);
+    .filter((definition) => definition.definitionId !== 0)
+    .sort((a, b) => (a.points > b.points ? 1 : -1));
+  console.log('4: ', definitionArray);
   // Add the real definition at the end
-  definitionArray.push(realDefinition);
+  definitionArray.push(...realDefinition);
+  console.log('5: ', definitionArray);
   return definitionArray;
 };
 
@@ -51,6 +70,10 @@ const Postgame = (props: PostgameProps): React.ReactElement => {
   // Generate a dictionary of playerId: username to make getSortedDefinitions more efficient
   const [playerDict] = useState<PlayerDictionary>(
     getPlayerDictionary(lobbyData.players),
+  );
+  const guesses = useRecoilValue(guessesState);
+  const [sortedGuesses] = useState(
+    getSortedDefinitions(lobbyData, guesses, playerDict),
   );
 
   return (
