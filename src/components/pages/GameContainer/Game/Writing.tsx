aac06bd2cd1a6fd14ae0d5c4236b381417f91fd6
Drew Ross <mail.drew.ross@gmail.com>
2021-01-29 12:51:42 -0800

    Implement Timer to hide submission form when timer is finished.

--- src/components/pages/GameContainer/Game/Writing.tsx
+++ src/components/pages/GameContainer/Game/Writing.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
-import { isHostState, lobbyState } from '../../../../state';
+import { isHostState, lobbySettingsState, lobbyState } from '../../../../state';
 import { Host } from '../../../common/Host';
 import { Player } from '../../../common/Player';
+import Timer from '../../../common/Timer/Timer';
 
 const Writing = (props: WritingProps): React.ReactElement => {
   const lobbyData = useRecoilValue(lobbyState);
   const isHost = useRecoilValue(isHostState);
+  const LobbySettings = useRecoilValue(lobbySettingsState);
   const [definition, setDefinition] = useState('');
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [timerDone, setTimerDone] = useState(false);
 
   // If Host, submit a default string. Remove when API doesn't require the host to submit a definition
   useEffect(() => {
@@ -40,6 +43,7 @@ const Writing = (props: WritingProps): React.ReactElement => {
         <h3>Your Word:</h3>
         <p>{lobbyData.word}</p>
       </div>
+      <Timer seconds={LobbySettings.seconds} timeUp={setTimerDone} />
       <Host>
         <p>Waiting for players to submit definitions...</p>
         <button onClick={() => props.handleSetPhase('GUESSING')}>
@@ -47,7 +51,7 @@ const Writing = (props: WritingProps): React.ReactElement => {
         </button>
       </Host>
       <Player>
-        {!isSubmitted && (
+        {!isSubmitted && !timerDone && (
           <form
             onSubmit={(e) => {
               handleSubmit(e, definition);
@@ -66,6 +70,7 @@ const Writing = (props: WritingProps): React.ReactElement => {
             <button disabled={definition.trim() === ''}>Submit</button>
           </form>
         )}
+        {!isSubmitted && timerDone && <p>Time&apos;s up!</p>}
         {isSubmitted && (
           <div>
             <p>Submitted:</p>
