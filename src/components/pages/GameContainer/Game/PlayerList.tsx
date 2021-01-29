12accb8a17e9575b022843bd9611cca763188301
Drew Ross <mail.drew.ross@gmail.com>
2021-01-28 17:44:50 -0800

    Filter out host from PlayerList.

--- src/components/pages/GameContainer/Game/PlayerList.tsx
+++ src/components/pages/GameContainer/Game/PlayerList.tsx
@@ -35,14 +35,17 @@ const PlayerList = (props: PlayerListProps): React.ReactElement => {
 
   return (
     <div className="player-list">
-      {lobbyData.players.map((player: PlayerItem) => {
-        return (
-          <p className={playerClassName(lobbyData, player)} key={player.id}>
-            {`${playerId === player.id ? '(you)' : ''} ${player.username}`},
-            score: {player.points}
-          </p>
-        );
-      })}
+      {console.log(lobbyData)}
+      {lobbyData.players
+        .filter((player: PlayerItem) => player.id !== lobbyData.host)
+        .map((player: PlayerItem) => {
+          return (
+            <p className={playerClassName(lobbyData, player)} key={player.id}>
+              {`${playerId === player.id ? '(you)' : ''} ${player.username}`},
+              score: {player.points}, info: {`${player.id} : ${lobbyData.host}`}
+            </p>
+          );
+        })}
     </div>
   );
 };
