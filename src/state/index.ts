f1672e88acc9b46b3fc7c626575a053d3ad0a823
Drew Ross <mail.drew.ross@gmail.com>
2021-02-01 11:17:26 -0800

    Add import/export guessState.

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,6 +1,13 @@
+import { guessesState } from './guessesState';
 import { isHostState } from './isHostState';
-import { lobbyState } from './lobbyState';
 import { lobbyCodeState } from './lobbyCodeState';
 import { lobbySettingsState } from './lobbySettingsState';
+import { lobbyState } from './lobbyState';
 
-export { isHostState, lobbyState, lobbyCodeState, lobbySettingsState };
+export {
+  isHostState,
+  lobbyState,
+  lobbyCodeState,
+  lobbySettingsState,
+  guessesState,
+};
