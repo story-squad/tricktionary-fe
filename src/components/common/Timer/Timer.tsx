bdf039c0f6b0dd3b006b14e29433ea5288bd708a
Drew Ross <mail.drew.ross@gmail.com>
2021-02-12 10:41:24 -0800

    Type cast seconds during deconstruction.

--- src/components/common/Timer/Timer.tsx
+++ src/components/common/Timer/Timer.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
 const Timer = (props: TimerProps): React.ReactElement => {
-  const { seconds, timeUp } = props;
+  const { timeUp } = props;
+  const seconds = Number(props.seconds);
   const [timer, setTimer] = useState(seconds);
 
   useEffect(() => {
-    if (Number(seconds) > 0) {
-      let time = Number(seconds);
+    if (seconds > 0) {
+      let time = seconds;
       const interval = setInterval(() => {
         if (time > 0) {
           time--;
@@ -21,7 +22,7 @@ const Timer = (props: TimerProps): React.ReactElement => {
 
   return (
     <>
-      {seconds !== undefined && Number(seconds) > 0 && (
+      {seconds !== undefined && seconds > 0 && (
         <div className="countdown-container">
           <div id="timer">
             <span className="time">{timer}</span>{' '}
