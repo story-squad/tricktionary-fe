ea96847879a56a84ce474ea7cc83bcd835e2d360
Drew Ross <mail.drew.ross@gmail.com>
2021-02-15 14:11:25 -0800

    Add separate render if the player didn't submit a definition.

--- src/components/pages/GameContainer/Game/Postgame.tsx
+++ src/components/pages/GameContainer/Game/Postgame.tsx
@@ -125,24 +125,40 @@ const Postgame = (props: PostgameProps): React.ReactElement => {
 const DefinitionResult = (props: DefinitionResultProps): React.ReactElement => {
   const { username, definition, points, guesses } = props.definitionResult;
   return (
-    <div className="definition-result">
-      <div className="vote-align">
-        <div className="author-box">
-          <span className="result-username">{username} </span>
-          <span>wrote:</span>
-        </div>
-        <p className="result-votes">{points} votes</p>
-      </div>
-      <p className="result-definition">{definition}</p>
-      <p className="who-voted-p">Who voted: </p>
-      <div className="who-voted-box">
-        {guesses.map((guess, key) => (
-          <div key={key} className="guess-names">
-            <p className="who-voted">{guess}</p>
+    <>
+      {definition !== '' ? (
+        // Player submitted a definition
+        <div className="definition-result">
+          <div className="vote-align">
+            <div className="author-box">
+              <span className="result-username">{username} </span>
+              <span>wrote:</span>
+            </div>
+            <p className="result-votes">{points} votes</p>
           </div>
-        ))}
-      </div>
-    </div>
+          <p className="result-definition">{definition}</p>
+          <p className="who-voted-p">Who voted: </p>
+          <div className="who-voted-box">
+            {guesses.map((guess, key) => (
+              <div key={key} className="guess-names">
+                <p className="who-voted">{guess}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        // Player didn't submit a definition
+        <div className="definition-result">
+          <div className="vote-align">
+            <div className="author-box">
+              <span className="result-username">{username} </span>
+            </div>
+            <p className="result-votes">oops</p>
+          </div>
+          <p className="result-definition">No submission!</p>
+        </div>
+      )}
+    </>
   );
 };
 
