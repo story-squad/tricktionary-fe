df9ea9d718c630c8b53ea6c1e4cdcefeed589483
Drew Ross <mail.drew.ross@gmail.com>
2021-02-15 12:09:59 -0800

    Add timer sync constant.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,4 @@
+// Max seconds for definition writing timer
 export const MAX_SECONDS = Number(process.env.MAX_SECONDS) || 120;
+// How often the host sends a 'synchronize' event to set player timers
+export const TIMER_SYNC_INTERVAL = Number(process.env.TIMER_SYNC_INTERVAL) || 2;
