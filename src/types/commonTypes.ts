7ee0270df773182312ad144dc12040492b62553e
Drew Ross <mail.drew.ross@gmail.com>
2021-02-16 16:03:03 -0800

    Add DecodedToken and remove Token interfaces.

--- src/types/commonTypes.ts
+++ src/types/commonTypes.ts
@@ -2,15 +2,11 @@ export interface HostPlayerProps {
   children: React.ReactNode;
 }
 
-export interface Token {
-  message: string;
-  player: {
-    created_at: string;
-    id: string;
-    jump_code: unknown | null;
-    last_played: string;
-    last_user_id: string;
-    name: null | string;
-    token: string;
-  };
+export interface DecodedToken {
+  exp: number;
+  ext: string;
+  iat: number;
+  lob: string;
+  pid: string;
+  sub: string;
 }
