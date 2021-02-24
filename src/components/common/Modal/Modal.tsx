0f14e3aaa2c4ec93cd5460fca5a832cf46a146e0
Drew Ross <mail.drew.ross@gmail.com>
2021-02-24 12:58:34 -0800

    Add header prop to Modal.

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Modal = (props: ModalProps): React.ReactElement => {
-  const { visible, message, handleConfirm, handleCancel } = props;
+  const { visible, message, header, handleConfirm, handleCancel } = props;
 
   return (
     <>
@@ -9,7 +9,7 @@ const Modal = (props: ModalProps): React.ReactElement => {
         <>
           <div className="modal">
             <div className="modal-content">
-              <h2>Before you go...</h2>
+              <h2>{header}</h2>
               <p>{message}</p>
               <div className="modal-buttons">
                 <button onClick={handleConfirm}>Okay</button>
@@ -29,6 +29,7 @@ export default Modal;
 interface ModalProps {
   visible: boolean;
   message: string;
+  header: string;
   handleConfirm: () => void | (() => (args: unknown) => void);
   handleCancel: () => void | (() => (args: unknown) => void);
 }
