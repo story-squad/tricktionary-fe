661caf5eee22f6f2679ceb12286710a56d7fe312
Drew Ross <mail.drew.ross@gmail.com>
2021-03-04 14:47:10 -0800

    Extract functions and interfaces from Postgame.

--- src/components/pages/GameContainer/Game/Postgame/Postgame.tsx
+++ src/components/pages/GameContainer/Game/Postgame/Postgame.tsx
@@ -8,77 +8,20 @@ import {
   showNewHostModalState,
 } from '../../../../../state';
 import {
+  DefinitionResultItem,
+  DefinitionResultProps,
   GuessItem,
-  LobbyData,
-  PlayerItem,
+  PlayerDictionary,
 } from '../../../../../types/gameTypes';
+import {
+  getPlayerDictionary,
+  getSortedDefinitions,
+} from '../../../../../utils/helpers';
 import { Host } from '../../../../common/Host';
 import { Modal } from '../../../../common/Modal';
 import { Player } from '../../../../common/Player';
 import { PlayerList } from '../../../../common/PlayerList';
 import { SetHost } from '../../../../common/SetHost';
-
-// Create a list of definitions, attach players who guessed for each, calculate point gains (UI only), add real definiton to the end
-const getSortedDefinitions = (
-  lobbyData: LobbyData,
-  guesses: GuessItem[],
-  playerDict: PlayerDictionary,
-): DefinitionResultItem[] => {
-  // Create a definition dictionary to easily map all player guesses to each definition
-  const definitions: DefinitionDictionary = {};
-  lobbyData.players.forEach((player) => {
-    if (player.id !== lobbyData.host) {
-      definitions[player.definitionId as number] = {
-        username: player.username,
-        playerId: player.id,
-        definition: player.definition,
-        definitionId: player.definitionId as number,
-        guesses: [],
-        points: 0,
-      };
-    }
-  });
-  // Add real definition
-  definitions[0] = {
-    username: `Real Definition for ${lobbyData.word}`,
-    playerId: '0',
-    definition: lobbyData.definition,
-    definitionId: 0,
-    guesses: [],
-    points: 0,
-  };
-  // Add player guesses to corresponding definitions and increment points earned
-  guesses.forEach((guess) => {
-    try {
-      definitions[guess.guess].guesses.push(playerDict[guess.player]);
-      definitions[guess.guess].points += 1;
-    } catch {
-      return;
-    }
-  });
-  // Get an array from the result that can be sorted and mapped in JSX
-  let definitionArray = Object.values(definitions);
-  // Grab the real definition to place at the end after the array is sorted
-  const realDefinition = definitionArray.filter(
-    (definition) => definition.definitionId === 0,
-  );
-  // Remove the real definition and sort by point values
-  definitionArray = definitionArray
-    .filter((definition) => definition.definitionId !== 0)
-    .sort((a, b) => (a.points > b.points ? 1 : -1));
-  // Add the real definition at the end
-  definitionArray.push(...realDefinition);
-  return definitionArray;
-};
-
-// Generate a dictionary of playerId: username to make getSortedDefinitions more efficient
-const getPlayerDictionary = (players: PlayerItem[]): PlayerDictionary => {
-  const dict: PlayerDictionary = {};
-  players.forEach((player) => {
-    dict[player.id] = player.username;
-  });
-  return dict;
-};
 
 const Postgame = (props: PostgameProps): React.ReactElement => {
   const {
@@ -247,25 +190,4 @@ interface PostgameProps {
   handleSetHost: (hostId: string, guesses: GuessItem[]) => void;
   handleSetFinale: () => void;
   handleRevealResults: (guesses: GuessItem[]) => void;
-}
-
-interface DefinitionResultProps {
-  definitionResult: DefinitionResultItem;
-}
-
-interface PlayerDictionary {
-  [Key: string]: string;
-}
-
-interface DefinitionDictionary {
-  [Key: number]: DefinitionResultItem;
-}
-
-interface DefinitionResultItem {
-  username: string;
-  playerId: string;
-  definition: string;
-  definitionId: number;
-  guesses: string[];
-  points: number;
 }
