0087e097ed4097b34c80b64072ed60ce5c69e3ce
Drew Ross <mail.drew.ross@gmail.com>
2021-03-08 13:22:06 -0800

    Add postReaction api function.

--- src/api/apiRequests.tsx
+++ src/api/apiRequests.tsx
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
+import { postApiReaction } from '../types/apiTypes';
 import { REACT_APP_API_URL } from '../utils/constants';
 
 export const getReactions = (): Promise<AxiosResponse> =>
@@ -6,3 +7,8 @@ export const getReactions = (): Promise<AxiosResponse> =>
 
 export const getWords = (): Promise<AxiosResponse> =>
   axios.get(`${REACT_APP_API_URL}/api/words/scoop/3`);
+
+export const postReaction = (
+  reaction: postApiReaction,
+): Promise<AxiosResponse> =>
+  axios.post(`${REACT_APP_API_URL}/api/definition-reactions`, reaction);
