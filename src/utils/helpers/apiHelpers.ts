b0439e3c64e4e44f1842f50c7d16f72013094b96
Drew Ross <mail.drew.ross@gmail.com>
2021-03-08 16:25:11 -0800

    Extract JSON.parse function from filter.

--- src/utils/helpers/apiHelpers.ts
+++ src/utils/helpers/apiHelpers.ts
@@ -5,11 +5,12 @@ import { REACTION_IDS } from '../constants';
 // GET reactions/emojis from API then filter using .env list
 export const getSelectedReactions = async (): Promise<ReactionItem[]> => {
   let emojis = [] as ReactionItem[];
+  const reactionIds = JSON.parse(REACTION_IDS);
   await getReactions()
     .then(
       (res) =>
         (emojis = res.data.available.filter((reaction: ReactionItem) =>
-          JSON.parse(REACTION_IDS).includes(reaction.id),
+          reactionIds.includes(reaction.id),
         )),
     )
     .catch((err) => console.log(err));
