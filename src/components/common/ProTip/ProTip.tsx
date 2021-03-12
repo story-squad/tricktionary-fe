4367e56d11c99c4ae24a1008793f45d672035889
Drew Ross <mail.drew.ross@gmail.com>
2021-03-12 15:20:58 -0800

    Fix issue with message updating.

--- src/components/common/ProTip/ProTip.tsx
+++ src/components/common/ProTip/ProTip.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import proTips from '../../../utils/text/proTips.json';
 
 const ProTip = (props: ProTipProps): React.ReactElement => {
-  const tipMessage = () => {
+  const [tipMessage] = useState(() => {
     if (props.message) {
       return props.message;
     } else {
       return proTips[Math.floor(Math.random() * proTips.length)];
     }
-  };
+  });
 
   return (
     <div className="pro-tip">
       <h3>Pro Tip:</h3>
-      <p>{tipMessage()}</p>
+      <p>{tipMessage}</p>
     </div>
   );
 };
