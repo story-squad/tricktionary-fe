1d743a9ce544d8a8b6417366c9159ec88b73bbde
Drew Ross <mail.drew.ross@gmail.com>
2021-03-12 15:50:23 -0800

    Simplify tipMessage initial value function.

--- src/components/common/ProTip/ProTip.tsx
+++ src/components/common/ProTip/ProTip.tsx
@@ -2,13 +2,9 @@ import React, { useState } from 'react';
 import proTips from '../../../utils/text/proTips.json';
 
 const ProTip = (props: ProTipProps): React.ReactElement => {
-  const [tipMessage] = useState(() => {
-    if (props.message) {
-      return props.message;
-    } else {
-      return proTips[Math.floor(Math.random() * proTips.length)];
-    }
-  });
+  const [tipMessage] = useState(
+    () => props.message || proTips[Math.floor(Math.random() * proTips.length)],
+  );
 
   return (
     <div className="pro-tip">
