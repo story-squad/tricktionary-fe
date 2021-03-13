9763caecc79aba47092c18b51bdce1a278826548
Drew Ross <mail.drew.ross@gmail.com>
2021-03-12 16:00:20 -0800

    Add comment to ProTip.

--- src/components/common/ProTip/ProTip.tsx
+++ src/components/common/ProTip/ProTip.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import proTips from '../../../utils/text/proTips.json';
 
 const ProTip = (props: ProTipProps): React.ReactElement => {
+  // Display custom message if defined or random message
   const [tipMessage] = useState(
     () => props.message || proTips[Math.floor(Math.random() * proTips.length)],
   );
