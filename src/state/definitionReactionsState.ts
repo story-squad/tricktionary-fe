5e3624de951c271ea5635a1841188935ded3605a
Drew Ross <mail.drew.ross@gmail.com>
2021-03-16 15:26:34 -0700

    Change definitionReactionsState default to empty object.

--- src/state/definitionReactionsState.ts
+++ src/state/definitionReactionsState.ts
@@ -3,5 +3,5 @@ import { ReactionsDictionary } from '../types/gameTypes';
 
 export const definitionReactionsState = atom<ReactionsDictionary>({
   key: 'definitionReactionsState',
-  default: [],
+  default: {},
 });
