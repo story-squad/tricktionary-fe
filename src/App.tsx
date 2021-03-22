b652bd1fe3421641f4e151738d528f56edd47a8c
Drew Ross <mail.drew.ross@gmail.com>
2021-03-22 12:09:44 -0700

    Add localStorage value and functionality to keep track of open tabs.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useBeforeunload } from 'react-beforeunload';
 import { Route, Switch } from 'react-router-dom';
 import { CookiePopup } from './components/common/CookiePopup';
 import { Footer } from './components/common/Footer';
 import { GameContainer } from './components/pages/GameContainer';
+import { useLocalStorage } from './hooks';
 
 const App = (): React.ReactElement => {
+  const [openTabs, setOpenTabs] = useLocalStorage('openTabs', 0);
+
+  // Keep track of the number of tabs players are using
+  useEffect(() => {
+    if (Number(openTabs) >= 0) {
+      setOpenTabs(Number(openTabs) + 1);
+    } else {
+      setOpenTabs(1);
+    }
+  }, []);
+
+  useBeforeunload(() => {
+    setOpenTabs(Number(openTabs) - 1);
+  });
+
   return (
     <div className="App">
       <CookiePopup />
