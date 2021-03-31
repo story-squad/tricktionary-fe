5b46682c2be6d504d12645ebc76319cb02ab439a
Drew Ross <mail.drew.ross@gmail.com>
2021-03-31 13:28:26 -0700

    add 3 tests for updateReactionCounts

--- src/utils/helpers/gameHelpers.test.ts
+++ src/utils/helpers/gameHelpers.test.ts
@@ -43,9 +43,9 @@ describe('addReaction', () => {
 });
 
 describe('updateReactionCounts', () => {
-  it('Returns a ReactionsDictionary with updated values given a valid array', () => {
+  it('returns a ReactionsDictionary with updated values given a valid array', () => {
     // Function input
-    const inputReactionsDict: ReactionsDictionary = {
+    const inputReactionsDict1: ReactionsDictionary = {
       15: {
         8: 0,
         13: 0,
@@ -57,7 +57,19 @@ describe('updateReactionCounts', () => {
         42: 0,
       },
     };
-    const inputReactionsList: GetReactionsItem[] = [
+    const inputReactionsDict2: ReactionsDictionary = {
+      15: {
+        8: 1,
+        13: 2,
+        42: 3,
+      },
+      16: {
+        8: 4,
+        13: 5,
+        42: 6,
+      },
+    };
+    const inputReactionsList1: GetReactionsItem[] = [
       {
         definition_id: 15,
         reaction_id: 8,
@@ -74,9 +86,41 @@ describe('updateReactionCounts', () => {
         count: 3,
       },
     ];
+    const inputReactionsList2: GetReactionsItem[] = [
+      {
+        definition_id: 15,
+        reaction_id: 8,
+        count: 7,
+      },
+      {
+        definition_id: 15,
+        reaction_id: 13,
+        count: 6,
+      },
+      {
+        definition_id: 15,
+        reaction_id: 42,
+        count: 3,
+      },
+      {
+        definition_id: 16,
+        reaction_id: 8,
+        count: 7,
+      },
+      {
+        definition_id: 16,
+        reaction_id: 13,
+        count: 6,
+      },
+      {
+        definition_id: 16,
+        reaction_id: 42,
+        count: 3,
+      },
+    ];
 
     // Expected output
-    const outputReactionsDict: ReactionsDictionary = {
+    const outputReactionsDict1: ReactionsDictionary = {
       15: {
         8: 7,
         13: 6,
@@ -88,13 +132,75 @@ describe('updateReactionCounts', () => {
         42: 0,
       },
     };
+    const outputReactionsDict2: ReactionsDictionary = {
+      15: {
+        8: 7,
+        13: 6,
+        42: 3,
+      },
+      16: {
+        8: 7,
+        13: 6,
+        42: 3,
+      },
+    };
 
     // Action
-    const output = updateReactionCounts(inputReactionsDict, inputReactionsList);
+    const output1 = updateReactionCounts(
+      inputReactionsDict1,
+      inputReactionsList1,
+    );
+    const output2 = updateReactionCounts(
+      inputReactionsDict2,
+      inputReactionsList2,
+    );
 
     // Assertion
-    expect(output).toEqual(outputReactionsDict);
+    expect(output1).toEqual(outputReactionsDict1);
+    expect(output2).toEqual(outputReactionsDict2);
   });
-  it('Returns the original ReactionsDictionary given an empty array', () => {});
-  it('Returns the original ReactionsDictionary given an invalid array', () => {});
+  it('returns the original ReactionsDictionary given an empty array', () => {
+    // Function input
+    const inputReactionsDict: ReactionsDictionary = {
+      15: {
+        8: 1,
+        13: 2,
+        42: 3,
+      },
+      16: {
+        8: 4,
+        13: 5,
+        42: 6,
+      },
+    };
+
+    // Action
+    const output = updateReactionCounts(inputReactionsDict, []);
+
+    // Assertion
+    expect(output).toEqual(inputReactionsDict);
+  });
+  it('returns the original ReactionsDictionary given an invalid array', () => {
+    // Function input
+    const inputReactionsDict: ReactionsDictionary = {
+      15: {
+        8: 1,
+        13: 2,
+        42: 3,
+      },
+      16: {
+        8: 4,
+        13: 5,
+        42: 6,
+      },
+    };
+    const inputReactionsList: any = [
+      [42, 'the meaning of life', { foo: 'bar', soap: 'bar', candy: 'bar' }],
+    ];
+    // Action
+    const output = updateReactionCounts(inputReactionsDict, []);
+
+    // Assertion
+    expect(output).toEqual(inputReactionsDict);
+  });
 });
