143bb15e933d68b96aa3511be3fab62ddc26c2bc
Drew Ross <mail.drew.ross@gmail.com>
2021-03-31 12:34:06 -0700

    add test for updateReactionCounts

--- src/utils/helpers/gameHelpers.test.ts
+++ src/utils/helpers/gameHelpers.test.ts
@@ -1,5 +1,5 @@
-import { ReactionsDictionary } from '../../types/gameTypes';
-import { addReaction } from './gameHelpers';
+import { GetReactionsItem, ReactionsDictionary } from '../../types/gameTypes';
+import { addReaction, updateReactionCounts } from './gameHelpers';
 
 describe('addReaction', () => {
   it('returns a ReactionsDictionary with an updated reaction value', () => {
@@ -20,7 +20,7 @@ describe('addReaction', () => {
     const reactionId = 102;
     const value = 22;
 
-    // Function output
+    // Expected output
     const outputReactions: ReactionsDictionary = {
       1000: {
         100: 5,
@@ -34,8 +34,67 @@ describe('addReaction', () => {
       },
     };
 
+    // Action
     const output = addReaction(inputReactions, definitionId, reactionId, value);
 
+    // Assertion
     expect(output).toEqual(outputReactions);
   });
+});
+
+describe('updateReactionCounts', () => {
+  it('Returns a ReactionsDictionary with updated values given a valid array', () => {
+    // Function input
+    const inputReactionsDict: ReactionsDictionary = {
+      15: {
+        8: 0,
+        13: 0,
+        42: 0,
+      },
+      16: {
+        8: 0,
+        13: 0,
+        42: 0,
+      },
+    };
+    const inputReactionsList: GetReactionsItem[] = [
+      {
+        definition_id: 15,
+        reaction_id: 8,
+        count: 7,
+      },
+      {
+        definition_id: 15,
+        reaction_id: 13,
+        count: 6,
+      },
+      {
+        definition_id: 15,
+        reaction_id: 42,
+        count: 3,
+      },
+    ];
+
+    // Expected output
+    const outputReactionsDict: ReactionsDictionary = {
+      15: {
+        8: 7,
+        13: 6,
+        42: 3,
+      },
+      16: {
+        8: 0,
+        13: 0,
+        42: 0,
+      },
+    };
+
+    // Action
+    const output = updateReactionCounts(inputReactionsDict, inputReactionsList);
+
+    // Assertion
+    expect(output).toEqual(outputReactionsDict);
+  });
+  it('Returns the original ReactionsDictionary given an empty array', () => {});
+  it('Returns the original ReactionsDictionary given an invalid array', () => {});
 });
